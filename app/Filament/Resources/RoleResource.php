4386bb9529b9fd39991a05266183e7d46c2689d4
bayurifkialghifari <bayurifkialgh@gmail.com>
2024-05-28 14:26:23 +0700

    move delete action

--- app/Filament/Resources/RoleResource.php
+++ app/Filament/Resources/RoleResource.php
@@ -37,6 +37,7 @@ class RoleResource extends Resource
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
